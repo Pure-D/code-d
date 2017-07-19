4205f9bf6854c941050f6a62d296df84047f3a9e
WebFreak001 <janju007@web.de>
2017-07-19 18:08:53 +0200

    Updated dub binaries to 1.3.0

--- src/installer.ts
+++ src/installer.ts
@@ -23,11 +23,11 @@ export function downloadDub(env) {
 	var url = "";
 	var ext = "";
 	if (process.platform == "linux" && process.arch == "x64") {
-		url = "https://code.dlang.org/files/dub-1.0.0-linux-x86_64.tar.gz";
+		url = "https://code.dlang.org/files/dub-1.3.0-linux-x86_64.tar.gz";
 		ext = ".tar.gz";
 	}
 	else if (process.platform == "linux" && process.arch == "ia32") {
-		url = "https://code.dlang.org/files/dub-1.0.0-linux-x86.tar.gz";
+		url = "https://code.dlang.org/files/dub-1.3.0-linux-x86.tar.gz";
 		ext = ".tar.gz";
 	}
 	else if (process.platform == "linux" && process.arch == "arm") {
@@ -35,11 +35,11 @@ export function downloadDub(env) {
 		ext = ".tar.gz";
 	}
 	else if (process.platform == "win32") {
-		url = "https://code.dlang.org/files/dub-1.0.0-windows-x86.zip";
+		url = "https://code.dlang.org/files/dub-1.3.0-windows-x86.zip";
 		ext = ".zip";
 	}
 	else if (process.platform == "darwin" && process.arch == "x64") {
-		url = "https://code.dlang.org/files/dub-1.0.0-osx-x86_64.tar.gz";
+		url = "https://code.dlang.org/files/dub-1.3.0-osx-x86_64.tar.gz";
 		ext = ".tar.gz";
 	}
 	else
