b7765e9b35eb560dae55f973780db7747fa6c586
WebFreak001 <janju007@web.de>
2017-08-13 02:01:32 +0200

    Support dscanner.ini completion #22

--- src/dmode.ts
+++ src/dmode.ts
@@ -6,4 +6,5 @@ export const DML_MODE: vscode.DocumentFilter = { language: "dml", scheme: "file"
 export const DSCRIPT_MODE: vscode.DocumentFilter = { language: "dscript", scheme: "file" };
 export const SDL_MODE: vscode.DocumentFilter = { language: "sdl", scheme: "file" };
 export const DUB_MODE: vscode.DocumentFilter = { pattern: path.join(vscode.workspace.rootPath, "dub.{sdl,json}"), scheme: "file" };
+export const DSCANNER_INI_MODE: vscode.DocumentFilter = { pattern: "**/dscanner.ini", scheme: "file" };
 export const DIET_MODE: vscode.DocumentFilter = { language: "diet", scheme: "file" };
