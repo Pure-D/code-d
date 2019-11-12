a0609094d3dbca0a34ce539e0700daa087e85b22
WebFreak001 <gh@webfreak.org>
2019-11-12 01:06:50 +0100

    cleanup & test diet syntax better

---
+++ ci/syntaxes.test.ts
@@ -0,0 +1,103 @@
+import * as assert from 'assert';
+import * as vscode from 'vscode';
+import * as fs from 'fs';
+import * as paths from 'path';
+import * as vsctm from 'vscode-textmate';
+import { suite, test } from 'mocha';
+
+function res(path: string): string {
+	return paths.join(__dirname, "../../ci", path);
+}
+
+function readFile(path: string): Promise<Buffer> {
+	return new Promise((resolve, reject) => {
+		fs.readFile(res(path), (error, data) => error ? reject(error) : resolve(data));
+	});
+}
+
+const registry = new vsctm.Registry({
+	loadGrammar: async (scopeName): Promise<vsctm.IRawGrammar | undefined | null> => {
+		if (scopeName === 'source.diet') {
+			const data = await readFile('../syntaxes/diet.json');
+			return vsctm.parseRawGrammar(data.toString(), '../syntaxes/diet.json');
+		}
+		else if (scopeName === 'source.d') {
+			const data = await readFile('../syntaxes/d.json');
+			return vsctm.parseRawGrammar(data.toString(), '../syntaxes/d.json');
+		}
+		else if (scopeName === 'source.dml') {
+			const data = await readFile('../syntaxes/dml.json');
+			return vsctm.parseRawGrammar(data.toString(), '../syntaxes/dml.json');
+		}
+		else if (scopeName === 'source.sdl') {
+			const data = await readFile('../syntaxes/sdl.json');
+			return vsctm.parseRawGrammar(data.toString(), '../syntaxes/sdl.json');
+		}
+		console.error(`Unknown scope name: ${scopeName}`);
+		return null;
+	}
+});
+
+function testSyntaxes(grammar: vsctm.IGrammar, folder: string, ext: string) {
+	return new Promise((resolve, reject) => {
+		if (!fs.existsSync(res(folder)))
+			return resolve(null);
+
+		fs.readdir(res(folder), async (err, files) => {
+			if (err)
+				return reject(err);
+
+			try {
+				for (let i = 0; i < files.length; i++) {
+					const file = files[i];
+
+					if (!file.endsWith(ext))
+						continue;
+
+					let ruleStack = vsctm.INITIAL;
+
+					const text = await readFile(paths.join(folder, file));
+					const lines = text.toString().split(/\r?\n/g);
+					const tokens = lines.map(line => grammar.tokenizeLine(line, ruleStack).tokens.map(a => {
+						return {
+							start: a.startIndex,
+							end: a.endIndex,
+							scope: a.scopes[a.scopes.length - 1]
+						};
+					}));
+
+					const actual = tokens.map(line => JSON.stringify(line)).join("\n");
+					fs.writeFileSync(res(paths.join(folder, file) + ".actual"), actual);
+
+					const expectedText = await readFile(paths.join(folder, file) + ".expected");
+					const expectedLines = expectedText.toString().split(/\r?\n/g);
+					const expectedTokens = expectedLines.map(line => JSON.parse(line));
+
+					assert.deepStrictEqual(tokens, expectedTokens);
+				}
+				resolve();
+			}
+			catch (e) {
+				reject(e);
+			}
+		});
+	});
+}
+
+suite("syntax tests", () => {
+	test("diet", () => {
+		return registry.loadGrammar('source.diet').then(grammar => {
+			return testSyntaxes(grammar, "syntax/diet", ".dt");
+		});
+	});
+	test("d", () => {
+		return registry.loadGrammar('source.d').then(grammar => {
+			return testSyntaxes(grammar, "syntax/d", ".d");
+		});
+	});
+	test("dml", () => {
+		return registry.loadGrammar('source.dml').then(grammar => {
+			return testSyntaxes(grammar, "syntax/dml", ".dml");
+		});
+	});
+});
