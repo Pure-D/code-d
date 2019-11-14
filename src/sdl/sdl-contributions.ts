f144b3a041b5221149cd2a0a90eb7e022c557c89
WebFreak001 <gh@webfreak.org>
2019-11-14 09:31:01 +0100

    type safe SDL dub completion + platforms fix #248

--- src/sdl/sdl-contributions.ts
+++ src/sdl/sdl-contributions.ts
@@ -1,5 +1,5 @@
-import { getLocationInfo } from "./sdlinfo"
-import { parseSDL, Tag, Value } from "./sdlparse"
+import { getLocationInfo, SDLCompletionInfo } from "./sdlinfo"
+import { parseSDL, Tag, Value, TagParseError } from "./sdlparse"
 import * as path from "path"
 import * as vscode from "vscode"
 import { listPackages, getPackageInfo, getLatestPackageInfo } from "../dub-api"
@@ -37,7 +37,7 @@ function pad3(n: number) {
 	return "00" + n.toString();
 }
 
-function completeDubVersion(info: any): any {
+function completeDubVersion(info: SDLCompletionInfo): SDLCompletionResult {
 	if (!info.currentSDLObject.values || info.currentSDLObject.values.length != 1)
 		return [];
 	var packageName = info.currentSDLObject.values[0].value;
@@ -70,11 +70,11 @@ function completeDubVersion(info: any): any {
 	});
 }
 
-function completeDubPackageName(info: any) {
+function completeDubPackageName(info: SDLCompletionInfo): SDLCompletionResult {
 	return new Promise((resolve) => {
-		var colonIdx = info.value.indexOf(":");
+		var colonIdx = info.partial.indexOf(":");
 		if (colonIdx != -1) {
-			var pkgName = info.value.substr(0, colonIdx);
+			var pkgName = info.partial.substr(0, colonIdx);
 			getLatestPackageInfo(pkgName).then(info => {
 				var results: vscode.CompletionItem[] = [];
 				if (info.subPackages)
@@ -113,6 +113,125 @@ function completeDubPackageName(info: any) {
 	});
 }
 
+const platforms = [
+	"windows",
+	"linux",
+	"posix",
+	"osx",
+	"freebsd",
+	"openbsd",
+	"netbsd",
+	"dragonflybsd",
+	"bsd",
+	"solaris",
+	"aix",
+	"haiku",
+	"skyos",
+	"sysv3",
+	"sysv4",
+	"hurd",
+	"android",
+	"cygwin",
+	"mingw",
+	"wasm",
+];
+const platformComplete = platforms.map(a => new vscode.CompletionItem(a, vscode.CompletionItemKind.Property));
+
+const architectures = [
+	"x86",
+	"x86_64",
+	"arm",
+	"aarch64",
+	"arm_thumb",
+	"arm_softfloat",
+	"arm_hardfloat",
+	"ppc",
+	"ppc_softfp",
+	"ppc_hardfp",
+	"ppc64",
+	"ia64",
+	"mips",
+	"mips32",
+	"mips64",
+	"mips_o32",
+	"mips_n32",
+	"mips_o64",
+	"mips_n64",
+	"mips_eabi",
+	"mips_nofloat",
+	"mips_softfloat",
+	"mips_hardfloat",
+	"sparc",
+	"sparc_v8plus",
+	"sparc_softfp",
+	"sparc_hardfp",
+	"sparc64",
+	"s390",
+	"s390x",
+	"hppa",
+	"hppa64",
+	"sh",
+	"sh64",
+	"alpha",
+	"alpha_softfp",
+	"alpha_hardfp"
+];
+const architectureComplete = architectures.map(a => new vscode.CompletionItem(a, vscode.CompletionItemKind.EnumMember));
+
+const compilers = ["dmd", "gdc", "ldc", "sdc"];
+const compilerComplete = compilers.map(a => new vscode.CompletionItem(a, vscode.CompletionItemKind.Method));
+
+function platformPatternCompleter(info: SDLCompletionInfo): SDLCompletionResult {
+	// os-architecture-compiler
+	let part = info.partial.trim();
+	let parts = part.split(/-/g);
+
+	if (parts.length > 0) {
+		// insert os into architecture or architecture-compiler
+		if (architectures.indexOf(parts[0]) != -1)
+			parts.unshift("");
+
+		// insert os-architecture into compiler
+		if (compilers.indexOf(parts[0]) != -1)
+			parts.unshift("", "");
+
+		// insert architecture into os-compiler
+		if (compilers.indexOf(parts[1]) != -1)
+			parts.splice(1, 0, "");
+	}
+
+	let result: vscode.CompletionItem[] = [];
+	if (parts.length == 0 || parts.length == 1) {
+		result.push.apply(result, platformComplete);
+		result.push.apply(result, architectureComplete);
+		result.push.apply(result, compilerComplete);
+	} else if (parts.length == 2) {
+		result.push.apply(result, architectureComplete);
+		result.push.apply(result, compilerComplete);
+	} else if (parts.length == 3) {
+		result = compilerComplete;
+	} else {
+		result = [];
+	}
+	return result;
+}
+
+const platformPattern: CompletionPattern = {
+	complete: platformPatternCompleter
+};
+
+const platformAttribute: CompletionAttribute = {
+	description: "Platform specifier to limit this tag or block to a given host platform. Platform attributes contain dash separated list of operating system/architecture/compiler identifiers, as defined in the D language reference, but converted to lower case. The order of these suffixes is os-architecture-compiler, where any of these parts can be left off.",
+	values: {
+		type: "string",
+		pattern: platformPattern
+	}
+};
+
+const platformAttributes: CompletionAttributeMap = {
+	platform: platformAttribute
+};
+
 const licenses = [
 	"public domain",
 	"proprietary",
@@ -140,7 +259,46 @@ const licenses = [
 	"Zlib"
 ];
 
-const packageName = {
+interface CompletionTag {
+	description: string;
+	values?: CompletionValues;
+	attributes?: CompletionAttributeMap;
+	minValues?: number;
+	maxValues?: number;
+	tags?: CompletionTagMap | null;
+	requireTags?: boolean;
+	namespace?: string;
+	namespaces?: string[];
+}
+
+interface CompletionValues {
+	type: "string" | "boolean" | "value" | "block";
+	pattern?: CompletionPattern | RegExp;
+	enum?: string[];
+	enumOptional?: boolean;
+}
+
+interface CompletionAttribute {
+	description: string;
+	values: CompletionValues;
+}
+
+interface CompletionPattern {
+	complete?: SDLCompletionCallback;
+	validate?: SDLValidationCallback;
+	exec?: SDLValidateExecCallback;
+}
+
+type CompletionTagMap = { [index: string]: CompletionTag };
+type OptionalCompletionTagMap = { [index: string]: CompletionTag | undefined };
+type CompletionAttributeMap = { [index: string]: CompletionAttribute };
+
+type SDLCompletionResult = Thenable<(vscode.CompletionItem | string)[]> | (vscode.CompletionItem | string)[];
+type SDLCompletionCallback = (info: SDLCompletionInfo) => SDLCompletionResult;
+type SDLValidationCallback = (value: Value) => string | undefined;
+type SDLValidateExecCallback = (value: Value) => boolean;
+
+const packageName: CompletionTag = {
 	description: 'Name of the package, used to uniquely identify the package. Must be comprised of only lower case ASCII alpha-numeric characters, "-" or "_".',
 	values: {
 		type: "string",
@@ -149,7 +307,7 @@ const packageName = {
 	minValues: 1,
 	maxValues: 1
 };
-const buildSettings = {
+const buildSettings: CompletionTagMap = {
 	dependency: {
 		description: "Adds a single dependency of the given name, attributes are used to configure the version/path to use - see next section for how version specifications look like. Use multiple dependency directives to add more than one dependency.",
 		values: {
@@ -264,6 +422,7 @@ const buildSettings = {
 				"noDefaultFlags"
 			]
 		},
+		attributes: platformAttributes,
 		minValues: 1
 	},
 	buildOptions: {
@@ -296,6 +455,7 @@ const buildSettings = {
 				"betterC"
 			]
 		},
+		attributes: platformAttributes,
 		minValues: 1
 	},
 	libs: {
@@ -303,6 +463,7 @@ const buildSettings = {
 		values: {
 			type: "string"
 		},
+		attributes: platformAttributes,
 		minValues: 1
 	},
 	sourceFiles: {
@@ -310,6 +471,7 @@ const buildSettings = {
 		values: {
 			type: "string"
 		},
+		attributes: platformAttributes,
 		minValues: 1
 	},
 	sourcePaths: {
@@ -317,6 +479,7 @@ const buildSettings = {
 		values: {
 			type: "string"
 		},
+		attributes: platformAttributes,
 		minValues: 1
 	},
 	excludedSourceFiles: {
@@ -324,6 +487,7 @@ const buildSettings = {
 		values: {
 			type: "string"
 		},
+		attributes: platformAttributes,
 		minValues: 1
 	},
 	mainSourceFile: {
@@ -339,6 +503,7 @@ const buildSettings = {
 		values: {
 			type: "string"
 		},
+		attributes: platformAttributes,
 		minValues: 1
 	},
 	versions: {
@@ -346,6 +511,7 @@ const buildSettings = {
 		values: {
 			type: "string"
 		},
+		attributes: platformAttributes,
 		minValues: 1
 	},
 	debugVersions: {
@@ -353,6 +519,7 @@ const buildSettings = {
 		values: {
 			type: "string"
 		},
+		attributes: platformAttributes,
 		minValues: 1
 	},
 	importPaths: {
@@ -360,6 +527,7 @@ const buildSettings = {
 		values: {
 			type: "string"
 		},
+		attributes: platformAttributes,
 		minValues: 1
 	},
 	stringImportPaths: {
@@ -367,6 +535,7 @@ const buildSettings = {
 		values: {
 			type: "string"
 		},
+		attributes: platformAttributes,
 		minValues: 1
 	},
 	preGenerateCommands: {
@@ -374,6 +543,7 @@ const buildSettings = {
 		values: {
 			type: "string"
 		},
+		attributes: platformAttributes,
 		minValues: 1
 	},
 	postGenerateCommands: {
@@ -381,6 +551,7 @@ const buildSettings = {
 		values: {
 			type: "string"
 		},
+		attributes: platformAttributes,
 		minValues: 1
 	},
 	preBuildCommands: {
@@ -388,6 +559,7 @@ const buildSettings = {
 		values: {
 			type: "string"
 		},
+		attributes: platformAttributes,
 		minValues: 1
 	},
 	postBuildCommands: {
@@ -395,6 +567,7 @@ const buildSettings = {
 		values: {
 			type: "string"
 		},
+		attributes: platformAttributes,
 		minValues: 1
 	},
 	dflags: {
@@ -402,6 +575,7 @@ const buildSettings = {
 		values: {
 			type: "string"
 		},
+		attributes: platformAttributes,
 		minValues: 1
 	},
 	lflags: {
@@ -409,11 +583,12 @@ const buildSettings = {
 		values: {
 			type: "string"
 		},
+		attributes: platformAttributes,
 		minValues: 1
 	}
 };
 
-function merge(a: any, b: any): any {
+function merge(a: OptionalCompletionTagMap, b: OptionalCompletionTagMap): CompletionTagMap {
 	var obj: any = {};
 	Object.keys(a).forEach(k => obj[k] = a[k]);
 	Object.keys(b).forEach(k => obj[k] = b[k]);
@@ -422,6 +597,7 @@ function merge(a: any, b: any): any {
 
 let dubSchema = {
 	title: "dub Package Schema",
+	description: "dub package file",
 	tags: merge(buildSettings, {
 		name: packageName,
 		description: {
@@ -460,19 +636,19 @@ let dubSchema = {
 			values: {
 				type: "string",
 				pattern: {
-					validate: function (value: Value) {
+					validate: function (value) {
 						if (value.value.trim().length == 0)
 							return "This value must be set";
 						return undefined;
 					},
-					complete: function (info: any) {
-						var words = info.value.trim().split(" ");
+					complete: function (info) {
+						var words = info.partial.trim().split(" ");
 						if (words.length == 0)
 							return licenses;
-						if (info.value[info.value.length - 1] != " ")
+						if (info.partial[info.partial.length - 1] != " ")
 							return licenses;
 						if (words[words.length - 1] == "or")
-							return ["later", "proprietary", ...licenses];
+							return ["later", ...licenses];
 						else
 							return ["or"];
 					}
@@ -495,6 +671,7 @@ let dubSchema = {
 			},
 			tags: merge(buildSettings, {
 				platforms: {
+					description: "A list of platform specifiers to limit on which platforms the configuration applies",
 					values: {
 						type: "string"
 					}
@@ -549,15 +726,15 @@ export class SDLContributions implements vscode.CompletionItemProvider {
 				return <any>Promise.reject("Invalid info object (length mismatch)");
 			if (info.namespace.length <= 0)
 				return <any>Promise.reject("Invalid info object (no entry)");
-			let obj: any = dubSchema;
+			let obj: CompletionTag = dubSchema;
 			let len = info.name.length - 1;
 			var found = true;
 			for (var i = 1; i < len; i++) {
 				found = false;
 				let name = info.name[i];
 				let namespace = info.namespace[i];
-				if (obj.tags[name] && (obj.tags[name].namespace || "") == namespace) {
-					obj = obj.tags[name];
+				if (obj.tags![name] && (obj.tags![name].namespace || "") == namespace) {
+					obj = obj.tags![name];
 					found = true;
 					continue;
 				}
@@ -568,47 +745,46 @@ export class SDLContributions implements vscode.CompletionItemProvider {
 			}
 			let completions: vscode.CompletionItem[] = [];
 			if (info.type == "block") {
-				Object.keys(obj.tags).forEach(key => {
-					let item = new vscode.CompletionItem(key);
-					item.documentation = obj.tags[key].description;
-					item.kind = vscode.CompletionItemKind.Field;
-					if (obj.tags[key].namespace)
-						item.insertText = new vscode.SnippetString().appendText(obj.tags[key].namespace + ":" + key);
-					else
-						item.insertText = new vscode.SnippetString().appendText(key);
-					completions.push(item);
-				});
+				if (obj.tags)
+					Object.keys(obj.tags).forEach(key => {
+						let item = new vscode.CompletionItem(key);
+						item.documentation = obj.tags![key].description;
+						item.kind = vscode.CompletionItemKind.Field;
+						if (obj.tags![key].namespace)
+							item.insertText = new vscode.SnippetString().appendText(obj.tags![key].namespace + ":" + key);
+						else
+							item.insertText = new vscode.SnippetString().appendText(key);
+						completions.push(item);
+					});
 			}
 			else if (info.type == "value") {
-				if (info.name[info.name.length - 1] != "") {
+				if (obj.attributes && info.name[info.name.length - 1] != "") {
 					obj = obj.attributes[info.name[info.name.length - 1]];
 				}
 				// single value
 				if (obj && obj.values) {
 					if (obj.values.pattern) {
-						if (obj.values.pattern.complete) {
-							return new Promise((resolve) => {
-								Promise.resolve(obj.values.pattern.complete(info)).then((values) => {
-									values.forEach((value: any) => {
-										if (typeof value == "object" && value instanceof vscode.CompletionItem)
-											completions.push(value);
-										else {
-											let item = new vscode.CompletionItem(value);
-											item.detail = obj.values.type;
-											item.kind = vscode.CompletionItemKind.Value;
-											item.insertText = new vscode.SnippetString().appendText(value);
-											completions.push(item);
-										}
-									});
-									resolve(completions);
+						if ((<any>obj.values.pattern).complete) {
+							return Promise.resolve((<any>obj.values!.pattern!).complete(info)).then((values) => {
+								values.forEach((value: any) => {
+									if (typeof value == "object" && value instanceof vscode.CompletionItem)
+										completions.push(value);
+									else {
+										let item = new vscode.CompletionItem(value);
+										item.detail = obj.values!.type;
+										item.kind = vscode.CompletionItemKind.Value;
+										item.insertText = new vscode.SnippetString().appendText(value);
+										completions.push(item);
+									}
 								});
+								return completions;
 							});
 						}
 					}
 					else if (obj.values.enum) {
 						obj.values.enum.forEach((value: any) => {
 							let item = new vscode.CompletionItem(value);
-							item.detail = obj.values.type;
+							item.detail = obj.values!.type;
 							item.kind = vscode.CompletionItemKind.Value;
 							item.insertText = new vscode.SnippetString().appendText(value);
 							completions.push(item);
@@ -631,11 +807,11 @@ export class SDLContributions implements vscode.CompletionItemProvider {
 				if (obj.attributes) {
 					Object.keys(obj.attributes).forEach(attribute => {
 						let item = new vscode.CompletionItem(attribute);
-						item.documentation = obj.attributes[attribute].description;
+						item.documentation = obj.attributes![attribute].description;
 						item.kind = vscode.CompletionItemKind.Variable;
 						var insertText = new vscode.SnippetString().appendText(attribute + "=");
-						if (obj.attributes[attribute].values) {
-							item.detail = obj.attributes[attribute].values.type;
+						if (obj.attributes![attribute].values) {
+							item.detail = obj.attributes![attribute].values.type;
 							if (item.detail == "string")
 								insertText.appendText('"').appendPlaceholder("").appendText('"');
 						}
@@ -677,15 +853,16 @@ export class SDLContributions implements vscode.CompletionItemProvider {
 				return new vscode.Range(0, 0, 0, 0);
 			return new vscode.Range(document.positionAt(r[0]), document.positionAt(r[1]));
 		}
-		root.errors.forEach((error: any) => {
-			errors.push(new vscode.Diagnostic(range(error.range), error.message, error.type == "error" ? vscode.DiagnosticSeverity.Error : vscode.DiagnosticSeverity.Warning));
-		});
-		function checkValue(value: Value, obj: any) {
+		if (root.errors)
+			root.errors.forEach((error: TagParseError) => {
+				errors.push(new vscode.Diagnostic(range(error.range), error.message, error.type == "error" ? vscode.DiagnosticSeverity.Error : vscode.DiagnosticSeverity.Warning));
+			});
+		function checkValue(value: Value, obj: CompletionValues) {
 			if (value.type != obj.type)
 				errors.push(new vscode.Diagnostic(range(value.range), "Type mismatch. Expected type: " + obj.type, vscode.DiagnosticSeverity.Error));
 			if (obj.pattern) {
-				if (typeof obj.pattern.validate == "function") {
-					var msg = obj.pattern.validate(value);
+				if (typeof (<any>obj.pattern).validate == "function") {
+					var msg = (<any>obj.pattern).validate(value);
 					if (msg)
 						errors.push(new vscode.Diagnostic(range(value.range), msg, vscode.DiagnosticSeverity.Error));
 				}
@@ -697,16 +874,16 @@ export class SDLContributions implements vscode.CompletionItemProvider {
 					errors.push(new vscode.Diagnostic(range(value.range), "This is not a valid value", vscode.DiagnosticSeverity.Error));
 			}
 		}
-		function scanTag(tag: Tag, obj: any, nsName = "") {
+		function scanTag(tag: Tag, obj: CompletionTag, nsName = "") {
 			if (obj.tags) {
 				var hasTags = false;
 				Object.keys(tag.tags).forEach(tagName => {
 					hasTags = true;
-					if (obj.tags[tagName])
+					if (obj.tags![tagName])
 						tag.tags[tagName].forEach(childTag => {
-							if (obj.tags[tagName].namespace && obj.tags[tagName].namespace != nsName && tag.range)
+							if (obj.tags![tagName].namespace && obj.tags![tagName].namespace != nsName && tag.range)
 								errors.push(new vscode.Diagnostic(range(tag.range), "Invalid namespace", vscode.DiagnosticSeverity.Error));
-							scanTag(childTag, obj.tags[tagName]);
+							scanTag(childTag, obj.tags![tagName]);
 						});
 				});
 				if (obj.requireTags && !hasTags && tag.range)
@@ -714,12 +891,12 @@ export class SDLContributions implements vscode.CompletionItemProvider {
 			}
 			if (obj.namespaces)
 				Object.keys(tag.namespaces).forEach(nsName => {
-					if (obj.namespaces.indexOf(nsName) != -1)
+					if (obj.namespaces!.indexOf(nsName) != -1)
 						scanTag(tag.namespaces[nsName], obj, nsName);
 				});
 			if (obj.values) {
 				tag.values.forEach(value => {
-					checkValue(value, obj.values);
+					checkValue(value, obj.values!);
 				});
 				if (typeof obj.minValues == "number")
 					if (tag.values.length < obj.minValues && tag.range)
@@ -730,9 +907,9 @@ export class SDLContributions implements vscode.CompletionItemProvider {
 			}
 			if (obj.attributes)
 				Object.keys(tag.attributes).forEach(attributeName => {
-					if (obj.attributes[attributeName])
+					if (obj.attributes![attributeName])
 						tag.attributes[attributeName].forEach(attribute => {
-							checkValue(attribute, obj.attributes[attributeName].values);
+							checkValue(attribute, obj.attributes![attributeName].values);
 						});
 				});
 		}
