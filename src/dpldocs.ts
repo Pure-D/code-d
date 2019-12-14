ee386fdf11b8abfb57814928d24e5e50219ad0f9
WebFreak001 <gh@webfreak.org>
2019-12-14 20:52:27 +0100

    refactor dpldocs search

--- src/dpldocs.ts
+++ src/dpldocs.ts
@@ -8,216 +8,57 @@ var DOMParser = require("xmldom").DOMParser;
 
 export type DocItem = vscode.QuickPickItem & { href: string, score: number, dependency?: DubDependencyInfo };
 
+class WorkState {
+	working: number = 0;
+	items: DocItem[] = [];
+	depItems: { [index: string]: DocItem[] } = {};
+
+	constructor(public quickPick: vscode.QuickPick<any>) {
+	}
+
+	startWork() {
+		this.working++;
+		this.quickPick.busy = this.working > 0;
+	}
+
+	finishWork() {
+		this.working--;
+		this.quickPick.busy = this.working > 0;
+	}
+
+	refreshItems() {
+		var ret: DocItem[] = this.items.slice();
+		for (var key in this.depItems)
+			if (this.depItems.hasOwnProperty(key))
+				ret.push.apply(ret, this.depItems[key]);
+		this.quickPick.items = ret;
+	}
+}
+
 export function showDpldocsSearch(query?: string) {
-
 	var quickpick = vscode.window.createQuickPick<DocItem>();
-	var items: DocItem[] = [];
-	var depItems: { [index: string]: DocItem[] } = {};
-	var working = 0;
+	const state = new WorkState(quickpick);
 
-	function refreshItems() {
-		var ret: DocItem[] = items.slice();
-		for (var key in depItems)
-			if (depItems.hasOwnProperty(key))
-				ret.push.apply(ret, depItems[key]);
-		quickpick.items = ret;
+	loadDependencyPackageDocumentations(state);
+
+	var timeout: NodeJS.Timer | undefined;
+	function updateSearch(query: string) {
+		timeout = updateRootSearchQuery(timeout, query, state);
 	}
-
-	if (served)
-		served.getChildren().then(deps => {
-			var checked: string[] = [];
-			deps.forEach(dep => {
-				if (dep.info) {
-					var strippedVersion = dep.info.version;
-					if (strippedVersion.startsWith("~"))
-						strippedVersion = strippedVersion.substr(1);
-
-					var strippedName = dep.info.name;
-					var colon = strippedName.indexOf(":");
-					if (colon != -1)
-						strippedName = strippedName.substr(0, colon);
-					if (checked.indexOf(strippedName) != -1)
-						return;
-					checked.push(strippedName);
-
-					var url = `https://${encodeURIComponent(strippedName)}.dpldocs.info/${encodeURIComponent(strippedVersion)}/search-results.html`;
-					var retried = false;
-					var doTry = function (dep: DubDependencyInfo, url: string) {
-						working++;
-						quickpick.busy = working > 0;
-						req()({ method: "GET", uri: url, gzip: true }, function (error: any, response: any, body: string) {
-							working--;
-							quickpick.busy = working > 0;
-							if (!error && response.statusCode == 200) {
-								if (response.headers["content-length"] || response.headers["Content-Length"]) {
-									var start = body.indexOf("<adrdox>");
-									if (start != -1) {
-										var end = body.indexOf("</adrdox>", start);
-										if (end != -1) {
-											var content = body.substring(start, end + "</adrdox>".length);
-											var xml: Document = new DOMParser().parseFromString(content, "text/xml");
-											var decls = xml.getElementsByTagName("decl");
-											var localItems: DocItem[] = [];
-											for (var j = 0; j < decls.length; j++) {
-												var declElem = decls[j];
-												console.log(declElem.childNodes.length);
-												var name: Element | null = null;
-												var link: Element | null = null;
-												var desc: Element | null = null;
-												for (var i = 0; i < declElem.childNodes.length; i++) {
-													var child = <any>declElem.childNodes[i];
-													if (child.tagName) {
-														if (child.tagName.toLowerCase() == "name")
-															name = child;
-														else if (child.tagName.toLowerCase() == "link")
-															link = child;
-														else if (child.tagName.toLowerCase() == "desc")
-															desc = child;
-													}
-												}
-												if (name && link) {
-													var href = (link.textContent || "").trim();
-													var m;
-													if (m = /\.(\d+)\.html/.exec(href))
-														if (parseInt(m[1]) > 1)
-															continue;
-													var obj: DocItem = {
-														dependency: dep,
-														label: strippedName + "/" + (name.textContent || "").replace(/<\/?.*?>/g, ""),
-														href: `https://${encodeURIComponent(strippedName)}.dpldocs.info/${encodeURIComponent(strippedVersion)}/${encodeURIComponent(href)}`,
-														score: 0
-													};
-													if (desc && desc.textContent)
-														obj.detail = desc.textContent.replace(/<\/?.*?>/g, "");
-													localItems.push(obj);
-												}
-											}
-											depItems[dep.name] = localItems;
-											refreshItems();
-										}
-									}
-								}
-								else if (!retried) {
-									retried = true;
-									doTry(dep, url);
-								}
-							}
-						});
-					}
-					doTry(dep.info, url);
-				}
-			});
-		});
-
-	function didChangeValue(value: string) {
-		clearTimeout(timeout);
-		timeout = setTimeout(() => {
-			working++;
-			quickpick.busy = working > 0;
-			req()("https://dpldocs.info/locate?q=" + encodeURIComponent(value), function (error: any, response: any, body: any) {
-				working--;
-				quickpick.busy = working > 0;
-				items = [];
-				if (!error && response.statusCode == 200) {
-					var dom = new JSDOM(body);
-					var results = <Element[]><any>dom.window.document.querySelectorAll("dt.search-result");
-					results.forEach(dt => {
-						var a = dt.querySelector("a");
-						if (a) {
-							var href = a.getAttribute("href");
-							if (href) {
-								var score = parseInt(dt.getAttribute("data-score") || "0");
-								var obj: DocItem = {
-									label: (a.textContent || "").replace(/\s+/g, ""),
-									href: href,
-									score: score
-								};
-
-								if (score > 0)
-									obj.description = "Search Score: " + score;
-
-								if (dt.nextElementSibling) {
-									obj.detail = (dt.nextElementSibling.textContent || "").replace(/([^\S\n]*\n[^\S\n]*\n[^\S\n]*)+/g, "\n\n");
-								}
-
-								items.push(obj);
-							}
-						}
-					});
-				}
-				refreshItems();
-			});
-		}, 500);
-	}
-
-	var timeout: NodeJS.Timer;
-	quickpick.onDidChangeValue(didChangeValue);
+	quickpick.onDidChangeValue((value) => updateSearch);
 
 	quickpick.placeholder = "Enter search term for symbol...";
 	quickpick.onDidAccept(() => {
 		var selection = quickpick.selectedItems[0];
-		if (selection) {
-			var panel = vscode.window.createWebviewPanel("dpldocs", selection.label, {
-				viewColumn: vscode.ViewColumn.Active
-			}, {
-				enableCommandUris: false,
-				enableFindWidget: true,
-				enableScripts: true,
-				localResourceRoots: []
-			});
-			var baseUri = selection.href;
-			panel.webview.onDidReceiveMessage((msg) => {
-				switch (msg.type) {
-					case "handle-link":
-						let href = path.posix.normalize(msg.href);
-						let uri = vscode.Uri.parse(baseUri);
-						if (href.startsWith("/")) {
-							baseUri = uri.with({
-								path: href
-							}).toString();
-						} else {
-							let file = uri.path;
-							let slash = file.lastIndexOf("/");
-							file = file.substring(0, slash + 1) + href;
-							baseUri = uri.with({
-								path: file
-							}).toString();
-						}
-						fillDplDocs(panel, msg.title, baseUri);
-						break;
-					case "open-module":
-						let module_ = <string>msg.module_;
-						let line = msg.line;
-
-						served.findFilesByModule(module_).then(files => {
-							if (!files.length) {
-								vscode.window.showErrorMessage("Could not find module " + module_);
-							}
-							else {
-								vscode.workspace.openTextDocument(files[0]).then(doc => {
-									vscode.window.showTextDocument(doc).then(editor => {
-										if (line > 0) {
-											var pos = new vscode.Position(line - 1, 0);
-											editor.revealRange(new vscode.Range(pos, pos), vscode.TextEditorRevealType.InCenter);
-											editor.selection.active = pos;
-										}
-									});
-								});
-							}
-						});
-
-						break;
-				}
-			});
-			fillDplDocs(panel, selection.label, selection.href);
-		}
+		if (selection)
+			showDocItemUI(selection);
 	});
-	quickpick.items = items;
+	quickpick.items = state.items;
 	quickpick.show();
 
 	if (query) {
 		quickpick.value = query;
-		didChangeValue(query);
+		updateSearch(query);
 	}
 }
 
@@ -425,6 +266,238 @@ export function fillDplDocs(panel: vscode.WebviewPanel, label: string, href: str
 					${page.innerHTML}
 				</body>
 				</html>`;
+		}
+	});
+}
+
+async function loadDependencyPackageDocumentations(state: WorkState) {
+	if (!served)
+		return;
+
+	let deps = await served.getChildren();
+	var checked: string[] = [];
+	deps.forEach(dep => {
+		if (dep.info) {
+			var strippedVersion = dep.info.version;
+			if (strippedVersion.startsWith("~"))
+				strippedVersion = strippedVersion.substr(1);
+
+			var strippedName = dep.info.name;
+			var colon = strippedName.indexOf(":");
+			if (colon != -1)
+				strippedName = strippedName.substr(0, colon);
+			if (checked.indexOf(strippedName) != -1)
+				return;
+			checked.push(strippedName);
+
+			state.startWork();
+			loadDependencySymbolsOnline(dep.info, strippedName, strippedVersion).then(docs => {
+				state.finishWork();
+				state.depItems[dep.info!.name] = docs;
+				state.refreshItems();
+			});
+		}
+	});
+}
+
+export function loadDependencySymbolsOnline(
+	dep: DubDependencyInfo | undefined,
+	strippedDependencyName: string,
+	strippedDependencyVersion: string): Thenable<DocItem[]> {
+	let url = `https://${encodeURIComponent(strippedDependencyName)}.dpldocs.info/${encodeURIComponent(strippedDependencyVersion)}/search-results.html`;
+
+	let retried = false;
+	let doTry = function (url: string) {
+		return new Promise<DocItem[]>((resolve, reject) => {
+			req()({ method: "GET", uri: url, gzip: true }, function (error: any, response: any, body: string) {
+				if (!error && response.statusCode == 200) {
+					if (response.headers["content-length"] || response.headers["Content-Length"]) {
+						resolve(parseDependencySearchResult(body, dep, strippedDependencyName, strippedDependencyVersion));
+					}
+					else if (!retried) {
+						retried = true;
+						return doTry(url);
+					}
+					else reject();
+				} else reject();
+			});
+		});
+	}
+	return doTry(url);
+}
+
+
+function updateRootSearchQuery(timeout: NodeJS.Timer | undefined, value: string, state: WorkState): NodeJS.Timer {
+	if (timeout !== undefined)
+		clearTimeout(timeout);
+	return setTimeout(() => {
+		state.startWork();
+		req()("https://dpldocs.info/locate?q=" + encodeURIComponent(value), function (error: any, response: any, body: any) {
+			state.finishWork();
+			state.items = [];
+			if (!error && response.statusCode == 200) {
+				let dom = new JSDOM(body);
+				let results = <Element[]><any>dom.window.document.querySelectorAll("dt.search-result");
+				results.forEach(dt => {
+					let item = parseDocItem(dt);
+					if (item)
+						state.items.push(item);
+				});
+			}
+			state.refreshItems();
+		});
+	}, 500);
+}
+
+function parseDependencySearchResult(
+	body: string,
+	dep: DubDependencyInfo | undefined,
+	strippedDependencyName: string,
+	strippedDependencyVersion: string): DocItem[] {
+	let start = body.indexOf("<adrdox>");
+	if (start == -1)
+		return [];
+	let end = body.indexOf("</adrdox>", start);
+	if (end == -1)
+		return [];
+
+	let content = body.substring(start, end + "</adrdox>".length);
+	let xml: Document = new DOMParser().parseFromString(content, "text/xml");
+	let decls = xml.getElementsByTagName("decl");
+	let localItems: DocItem[] = [];
+	for (let j = 0; j < decls.length; j++) {
+		let docEntry = parseDocEntry(decls[j]);
+		if (docEntry.name && docEntry.link) {
+			let href = docEntry.link;
+			let m;
+			if (m = /\.(\d+)\.html/.exec(href))
+				if (parseInt(m[1]) > 1)
+					continue;
+			let obj: DocItem = {
+				dependency: dep,
+				label: strippedDependencyName + "/" + docEntry.name,
+				href: `https://${encodeURIComponent(strippedDependencyName)}.dpldocs.info/${encodeURIComponent(strippedDependencyVersion)}/${encodeURIComponent(href)}`,
+				score: 0
+			};
+			if (docEntry.desc)
+				obj.detail = docEntry.desc;
+			localItems.push(obj);
+		}
+	}
+	return localItems;
+}
+
+interface DocEntry {
+	name: string | null;
+	link: string | null;
+	desc: string | null;
+}
+
+function parseDocEntry(declElem: Element): DocEntry {
+	console.log(declElem.childNodes.length);
+	let name: Element | null = null;
+	let link: Element | null = null;
+	let desc: Element | null = null;
+	for (let i = 0; i < declElem.childNodes.length; i++) {
+		let child = <any>declElem.childNodes[i];
+		if (child.tagName) {
+			if (child.tagName.toLowerCase() == "name")
+				name = child;
+			else if (child.tagName.toLowerCase() == "link")
+				link = child;
+			else if (child.tagName.toLowerCase() == "desc")
+				desc = child;
+		}
+	}
+	return {
+		name: getCleanSimpleTextContent(name),
+		link: getCleanSimpleTextContent(link),
+		desc: getCleanSimpleTextContent(desc)
+	};
+}
+
+function parseDocItem(dt: Element): DocItem | undefined {
+	let a = dt.querySelector("a");
+	if (!a)
+		return;
+	let href = a.getAttribute("href");
+	if (!href)
+		return;
+
+	let score = parseInt(dt.getAttribute("data-score") || "0");
+	let obj: DocItem = {
+		label: (a.textContent || "").replace(/\s+/g, ""),
+		href: href,
+		score: score
+	};
+
+	if (score > 0)
+		obj.description = "Search Score: " + score;
+
+	if (dt.nextElementSibling)
+		obj.detail = (dt.nextElementSibling.textContent || "").replace(/([^\S\n]*\n[^\S\n]*\n[^\S\n]*)+/g, "\n\n");
+
+	return obj;
+}
+
+function getCleanSimpleTextContent(elem: Element | null): string | null {
+	return elem ? (elem.textContent || "").replace(/<\/?.*?>/g, "").trim() : elem;
+}
+
+function showDocItemUI(docItem: DocItem) {
+	var panel = vscode.window.createWebviewPanel("dpldocs", docItem.label, {
+		viewColumn: vscode.ViewColumn.Active
+	}, {
+		enableCommandUris: false,
+		enableFindWidget: true,
+		enableScripts: true,
+		localResourceRoots: []
+	});
+	var baseUri = docItem.href;
+	panel.webview.onDidReceiveMessage((msg) => {
+		switch (msg.type) {
+			case "handle-link":
+				let href = path.posix.normalize(msg.href);
+				let uri = vscode.Uri.parse(baseUri);
+				if (href.startsWith("/")) {
+					baseUri = uri.with({
+						path: href
+					}).toString();
+				} else {
+					let file = uri.path;
+					let slash = file.lastIndexOf("/");
+					file = file.substring(0, slash + 1) + href;
+					baseUri = uri.with({
+						path: file
+					}).toString();
+				}
+				fillDplDocs(panel, msg.title, baseUri);
+				break;
+			case "open-module":
+				let module_ = <string>msg.module_;
+				let line = msg.line;
+				focusModule(module_, line);
+				break;
+		}
+	});
+	fillDplDocs(panel, docItem.label, docItem.href);
+}
+
+function focusModule(module_: string, line: number) {
+	served.findFilesByModule(module_).then(files => {
+		if (!files.length) {
+			vscode.window.showErrorMessage("Could not find module " + module_);
+		}
+		else {
+			vscode.workspace.openTextDocument(files[0]).then(doc => {
+				vscode.window.showTextDocument(doc).then(editor => {
+					if (line > 0) {
+						var pos = new vscode.Position(line - 1, 0);
+						editor.revealRange(new vscode.Range(pos, pos), vscode.TextEditorRevealType.InCenter);
+						editor.selection.active = pos;
+					}
+				});
+			});
 		}
 	});
 }
