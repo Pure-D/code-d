82372f9d4965c05157a48798238bcb4a20472469
WebFreak001 <gh@webfreak.org>
2020-06-05 18:12:56 +0200

    improve synchronized statements partially fix #288

---
+++ src/test/ci/syntax/d/issue_288.d
@@ -0,0 +1,12 @@
+void main() {
+	synchronized int x = 4;
+	synchronized stateInfo.status = "can't connect (%s)".format(addr);
+	return;
+	synchronized (foo) {}
+	int x;
+}
+
+synchronized class Foo {
+	synchronized SomeFunction!(int) foo() {}
+	synchronized int foo();
+}
