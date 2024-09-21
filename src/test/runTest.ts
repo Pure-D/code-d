f1c6fde3b2598240348b3a16bd471113c02985a9
WebFreak001 <gh@webfreak.org>
2024-09-22 00:45:35 +0200

    add basic integration test framework

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -1,22 +1,62 @@
-import * as path from 'path';
+import * as path from "path";
+import * as fs from "fs";
+import * as os from "os";
+import * as cp from "child_process";
 
-import { runTests } from 'vscode-test';
+const packageJson = require("../../package.json");
+
+import {
+  downloadAndUnzipVSCode,
+  resolveCliArgsFromVSCodeExecutablePath,
+  runTests,
+} from "@vscode/test-electron";
+import { rimraf } from "rimraf";
 
 async function main() {
   try {
     // The folder containing the Extension Manifest package.json
     // Passed to `--extensionDevelopmentPath`
-    const extensionDevelopmentPath = path.resolve(__dirname, '../../../');
+    const extensionDevelopmentPath = path.resolve(__dirname, "../../");
 
     // The path to the extension test runner script
     // Passed to --extensionTestsPath
-    const extensionTestsPath = path.resolve(__dirname, './suite/index');
+    const extensionTestsPath = path.resolve(__dirname, "./suite/index");
+
+    const vscodeExecutablePath = await downloadAndUnzipVSCode();
+
+    const [cliPath, ...args] =
+      resolveCliArgsFromVSCodeExecutablePath(vscodeExecutablePath);
+
+    // for (const extensionId of packageJson.extensionDependencies) {
+    //   cp.spawnSync(cliPath, [...args, "--install-extension", extensionId], {
+    //     encoding: "utf-8",
+    //     stdio: "inherit",
+    //   });
+    // }
+
+    await rimraf(".vscode-test/user-data");
+
+    let cwd = fs.mkdtempSync(path.join(os.tmpdir(), "coded_project"));
+    fs.writeFileSync(path.join(cwd, "dub.sdl"), 'name "codedproject"\n');
+    fs.mkdirSync(path.join(cwd, "source"));
+    fs.writeFileSync(
+      path.join(cwd, "source", "app.d"),
+      'import std.stdio;\n\nvoid main() {\n\twriteln("hello world");\n}\n'
+    );
 
     // Download VS Code, unzip it and run the integration test
-    await runTests({ extensionDevelopmentPath, extensionTestsPath });
+    await runTests({
+      vscodeExecutablePath,
+      extensionDevelopmentPath,
+      launchArgs: [cwd],
+      extensionTestsPath,
+      extensionTestsEnv: {
+        PROJECT_DIR: cwd,
+      },
+    });
   } catch (err) {
     console.error(err);
-    console.error('Failed to run tests');
+    console.error("Failed to run tests");
     process.exit(1);
   }
 }
